9dc66661a9437f1aac199d5ef2c20f55f147286a
Ahmadnawasrah <>
2021-04-14 12:06:03 +0000

    Javasecript

---
+++ JS/First.js
@@ -0,0 +1,27 @@
+
+alert("Welcome to may own shop " );
+
+var gender = prompt('pleas enter your gender ' );
+ 
+
+
+var imageMail='<img src="boy.jfif" alt="male" />'
+
+
+var imagefMail='<img src="girl.jfif" alt="female"/>'
+if (gender==='male')
+{
+
+document.write(imageMail);
+
+}
+else if(gender==="Female")  {
+
+document.write(imagefMail);
+
+
+}
+
+alert(" you can contact us all the time ")
+
+
